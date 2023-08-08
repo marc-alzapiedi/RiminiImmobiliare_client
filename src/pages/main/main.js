c0168dc10f44492a86a8cfcfdab0ca1e623295c1
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-08-08 18:07:05 +0200

    created some elements, needs to be finished and some styling needs to be added

---
+++ src/pages/main/main.js
@@ -0,0 +1,67 @@
+import './style.css'
+
+const Main = () => {
+
+    return (
+        <>
+            <div className="container">
+                <section className="top-header">
+                    <header>
+                        <nav>
+                            <a href=''>Agenzie</a>
+                            <a href=''>Prezzi immobili</a>
+                            <a href=''>Vendi casa</a>
+                            <a href=''>Valuta casa</a>
+                            <a href=''>Mutui</a>
+                        </nav>
+
+                        <div>
+                            <button>
+                                PUBBLICA ANNUNCI 
+                            </button>
+                            <a href=''> Accedi</a>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="800px" height="800px" viewBox="0 0 24 24" fill="none">
+                            <path d="M12.12 12.78C12.05 12.77 11.96 12.77 11.88 12.78C10.12 12.72 8.71997 11.28 8.71997 9.50998C8.71997 7.69998 10.18 6.22998 12 6.22998C13.81 6.22998 15.28 7.69998 15.28 9.50998C15.27 11.28 13.88 12.72 12.12 12.78Z" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M18.74 19.3801C16.96 21.0101 14.6 22.0001 12 22.0001C9.40001 22.0001 7.04001 21.0101 5.26001 19.3801C5.36001 18.4401 5.96001 17.5201 7.03001 16.8001C9.77001 14.9801 14.25 14.9801 16.97 16.8001C18.04 17.5201 18.64 18.4401 18.74 19.3801Z" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <i className='arrow down'></i>
+                        </div>
+                    </header>
+
+                    <main>
+                        <h1>                            
+                            Rimini Immobiliare
+                        </h1>
+                    </main>
+
+                    <nav>
+                        
+                    </nav>
+
+                </section>
+
+                <section className='body_1'>
+                    Ex esse deserunt duis velit excepteur labore voluptate sit elit deserunt. Pariatur culpa qui aute commodo in in dolor do. Lorem non in id dolore enim duis eu ullamco. Duis officia sit qui dolor excepteur qui. Nostrud adipisicing sint eu magna laborum commodo culpa enim irure do sint elit voluptate. Incididunt commodo officia eiusmod ut proident labore in. Irure nostrud anim do aute laboris dolor laborum.
+                </section>
+
+                <section className='body_2'>
+                    Non enim aliqua quis Lorem dolor adipisicing ea. Voluptate cillum ex culpa quis commodo aliqua elit consectetur dolore incididunt aliquip ullamco. Amet laborum veniam fugiat incididunt et sunt cillum ex duis ex. Laborum duis deserunt ullamco ullamco ad est aute magna laboris amet sunt esse.
+
+                </section>
+
+                <section className='bottom_footer'>
+                    Dolore excepteur anim exercitation aliqua. Laboris esse et consequat laboris aliquip pariatur pariatur ad est deserunt duis sunt nisi. Qui qui pariatur sint cupidatat commodo sint velit do reprehenderit laboris ad est commodo quis. Amet sit duis enim est. Ullamco eu reprehenderit esse mollit velit ut nisi consequat sint mollit ad.
+
+                </section>
+
+                <footer>
+                    Ea sit mollit exercitation id ut dolor occaecat commodo aute nulla. Ea aliqua adipisicing deserunt culpa esse commodo enim dolor excepteur ea. Aliquip aliquip do elit duis veniam sit cupidatat cupidatat. Tempor anim ea eu laboris enim minim veniam sunt occaecat ut et sunt aute. Veniam adipisicing non do nisi laborum. Ullamco labore tempor incididunt aliquip ad. Nulla voluptate consectetur consectetur laborum non non tempor.
+                </footer>
+
+            </div>
+        </>
+    )
+}
+
+export default Main
