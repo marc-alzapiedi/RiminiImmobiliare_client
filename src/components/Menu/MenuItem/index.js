8d6185cf0bccef7d792cdf5d5fb432639cb5bbed
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-09-16 21:06:17 +0200

    working on login

--- src/components/Menu/MenuItem/index.js
+++ src/components/Menu/MenuItem/index.js
@@ -1,17 +1,25 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 import ArrowRight from "../../../Icons/ArrowRight"
+import './style.css'
+
 
 const MenuItem = ({icon, text, children}) => {
 
+    const [isOpen, setIsOpen] = useState(false)
 
-    
+    const toggleIsOpen = () => {
+        setIsOpen(!isOpen)
+    }
 
     return (
-        <li>
 
+        
+        
+        <li>
             {!icon ? children : icon.type.name === 'ProfileIcon' ?
-            
-            <button>
+                
+            <button onClick={toggleIsOpen}> 
                 <p>
                 {icon}
                 </p>
@@ -19,25 +27,68 @@ const MenuItem = ({icon, text, children}) => {
                 {text}
                 </p>
             </button>
-             : 
+            : 
             icon.type.name !== 'ProfileIcon' ? 
             <Link>
                 {icon}
                 {children ? children : <p>{text}</p>}
                 {text !== 'Accedi / Registrati' && <ArrowRight />}
             </Link> : children
+                
+        }
+        {
+            isOpen && 
+            <div className="loginRegisterForm">
+                <div>
+                    <div>
+                        <p>
+                            Con il tuo account puoi:
+                        </p>
+                        <ul>
+                            <li>Salvare le tue ricerche</li>
+                            <li>Salvare gli annunci migliori</li>
+                            <li>Contattare con più facilità le agenzie</li>
+                            <li>Ricevere aggiornamenti personalizzati</li>
+                        </ul>
+                    </div>
+
+                </div>
+
+                <form>
+
+                    <div>
+                        <p>Accedi o registrati</p>
+                        <button>
+                            X
+                        </button>
+
+                    </div>
+
+
+                    <button>
+                        CONTINUA CON GOOGLE
+                    </button>
+
+                    <button>
+                        CONTINUA CON APPLE
+                    </button>
+
+                    <button>
+                        CONTINUA CON FACEBOOK
+                    </button>
+
+                    <p>Oppure</p>
+
+                    <input type="email"/>
+
+                </form>
+            </div>
+        }
             
-            }
-            {/* {
-            icon ? 
-            <Link>
-                {icon}
-                {children ? children : <p>{text}</p>}
-                {text !== 'Accedi / Registrati' && <ArrowRight />}
-            </Link> : children
-            } */}
-             
+                
         </li>
+
+
     )
 }
 
